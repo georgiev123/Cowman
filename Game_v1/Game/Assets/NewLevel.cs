f302806c6e59043db47c677279b7248760c8c6ff
Димитър Дамянов <you@example.comyou@example.com>
2018-03-21 20:19:39 +0200

    improoved patrooling, bullet spamming, enemy dieing

---
+++ Game_v1/Game/Assets/NewLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewLevel : MonoBehaviour {
+
+
+void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "colliderForBullets")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+
+    }
+}
