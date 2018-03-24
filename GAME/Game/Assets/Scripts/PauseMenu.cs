59a3a98411008a1157091d93a8af776ed310b3b9
Tsenko Tsenkov <tseni.tsenkov@gmail.com>
2018-03-24 18:41:25 +0200

    bugfix

--- GAME/Game/Assets/Scripts/PauseMenu.cs
+++ GAME/Game/Assets/Scripts/PauseMenu.cs
@@ -41,6 +41,7 @@ public class PauseMenu : MonoBehaviour {
 
     public void Quit()
     {
+		Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenuScene");
         Debug.Log("Quit!");
     }
